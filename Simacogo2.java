ec7fb1f323717b193016f03ddf8e27603b60eb9b
Reigningsun <38302744+Reigningsun@users.noreply.github.com>
2018-06-08 15:43:31 -0500

    Add files via upload

---
+++ Simacogo2.java
@@ -0,0 +1,248 @@
+package simacogo;
+
+public class Simacogo2{
+	byte [][] CURRSTATE;																			// Area of Play
+	byte PARENTSCORE;																				// Parents Score
+	byte SCORE;																						// Stores the evaluated score. AI wants positive values and human wants negative values
+	byte ROW;																						// Stores row of move taken to get to this game state
+	byte COL;																						// Stores column of move taken to get to this game state
+
+
+
+	
+	Simacogo2(byte gameState [][], byte parentscore, byte moveRow, byte moveCol){					// Constructor
+		this.CURRSTATE 	= gameState;
+		this.ROW		= moveRow;
+		this.COL		= moveCol;
+		this.PARENTSCORE= parentscore;
+		this.SCORE		= updateScore();
+		
+	}
+		
+
+	
+	// ** Print out Current State of Board ** 
+	
+	void printBoard(){
+		System.out.println(" 0  1  2  3  4  5  6  7  8 ");
+		for (int row = 8; row >= 0 ; row--){
+			for (int column = 0; column < 9; column++){
+				System.out.print('|');
+				if(CURRSTATE[row][column] == 9){
+					System.out.print('_');
+				} else {
+					System.out.print(CURRSTATE[row][column]);
+				}
+				System.out.print('|');
+			}
+			System.out.print(" ");
+			System.out.println(row);
+		}
+		System.out.println();
+	}
+	
+	
+	
+	// ** Methods for Scoring **  super ugly but fast -- if I get the time I will clean this up  
+	
+	byte updateScore (){																			// Combines points earned by this moves with the score of the game
+		return (byte) (PARENTSCORE + getMoveScore());
+	}
+	
+	byte getMoveScore (){
+		byte mScore = 0;																			// Adjacent moves gain 2 points, diagonal moves gain 1 point
+		int token = CURRSTATE[ROW][COL];															// Current players token
+		
+		
+		if (ROW > 0 && ROW < 8 && COL > 0 && COL < 8){												// Move is not an edge tile so we can look at all adjacent spaces
+			if (CURRSTATE[ROW -1][COL]	 	== token){ mScore = (byte) (mScore + 2); }				
+			if (CURRSTATE[ROW][COL -1] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW][COL +1] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW -1][COL -1] 	== token){ mScore = (byte) (mScore + 1); }			
+			if (CURRSTATE[ROW +1][COL +1] 	== token){ mScore = (byte) (mScore + 1); }			
+			if (CURRSTATE[ROW -1][COL +1] 	== token){ mScore = (byte) (mScore + 1); }			
+			if (CURRSTATE[ROW +1][COL -1] 	== token){ mScore = (byte) (mScore + 1); }	
+			if (token == 1){ return mScore;}       else{ return (byte) (0 - mScore); }
+			
+		}
+		else if (ROW == 0 && COL != 0 && COL != 8){													// Move is the bottom row but not in the corners
+			if (CURRSTATE[ROW][COL -1] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW][COL +1] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW +1][COL +1] 	== token){ mScore = (byte) (mScore + 1); }			
+			if (CURRSTATE[ROW +1][COL -1] 	== token){ mScore = (byte) (mScore + 1); }		
+			if (token == 1){ return mScore;}       else{ return (byte) (0 - mScore); }
+		}
+		else if (ROW == 0 && COL == 0){																// Move is bottom left corner		
+			if (CURRSTATE[ROW][COL +1] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW +1][COL +1] 	== token){ mScore = (byte) (mScore + 1); }			
+			if (token == 1){ return mScore;}       else{ return (byte) (0 - mScore); }
+		}
+		else if (ROW == 0 && COL == 8){																// Move is bottom right corner
+			if (CURRSTATE[ROW][COL -1] 		== token){ mScore = (byte) (mScore + 2); }					
+			if (CURRSTATE[ROW +1][COL -1] 	== token){ mScore = (byte) (mScore + 1); }			
+			if (token == 1){ return mScore;}       else{ return (byte) (0 - mScore); }
+		}
+		else if (ROW == 8 && COL != 0 && COL != 8){													// Move is in top row but not in a corner
+			if (CURRSTATE[ROW -1][COL] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW][COL -1] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW][COL +1] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW -1][COL -1] 	== token){ mScore = (byte) (mScore + 1); }			
+			if (CURRSTATE[ROW -1][COL +1] 	== token){ mScore = (byte) (mScore + 1); }			
+			if (token == 1){ return mScore;}       else{ return (byte) (0 - mScore); }
+		}
+		else if (ROW == 8 && COL == 0){																// Move is in top left corner
+			if (CURRSTATE[ROW][COL +1] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW -1][COL]	 	== token){ mScore = (byte) (mScore + 2); }			 
+			if (CURRSTATE[ROW -1][COL +1] 	== token){ mScore = (byte) (mScore + 1); }			
+			if (token == 1){ return mScore;}       else{ return (byte) (0 - mScore); }
+			}
+		else if (ROW == 8 && COL == 8){																// Move is in top right corner
+			if (CURRSTATE[ROW][COL -1] 		== token){ mScore = (byte) (mScore + 2); }			
+			if (CURRSTATE[ROW -1][COL] 		== token){ mScore = (byte) (mScore + 2); }			 
+			if (CURRSTATE[ROW -1][COL -1] 	== token){ mScore = (byte) (mScore + 1); }	
+			if (token == 1){ return mScore;}       else{ return (byte) (0 - mScore); }
+		}
+		else if (ROW != 0 && ROW != 8 && COL == 0){													// Move is on left edge but not in a corner
+			if (CURRSTATE[ROW -1][COL] 		== token){ mScore = (byte) (mScore + 2); }
+			if (CURRSTATE[ROW][COL +1] 		== token){ mScore = (byte) (mScore + 2); }
+			if (CURRSTATE[ROW +1][COL +1] 	== token){ mScore = (byte) (mScore + 1); }
+			if (CURRSTATE[ROW -1][COL +1] 	== token){ mScore = (byte) (mScore + 1); }
+			if (token == 1){ return mScore;}       else{ return (byte) (0 - mScore); }
+		} else {																					// Move is on the right edge but not in a corner
+			if (CURRSTATE[ROW -1][COL] 		== token){ mScore = (byte) (mScore + 2); }
+			if (CURRSTATE[ROW][COL -1] 		== token){ mScore = (byte) (mScore + 2); }
+			if (CURRSTATE[ROW +1][COL -1] 	== token){ mScore = (byte) (mScore + 1); }
+			if (CURRSTATE[ROW -1][COL -1] 	== token){ mScore = (byte) (mScore + 1); }
+			if (token == 1){ return mScore;}       else{ return (byte) (0 - mScore); }
+		}
+	}
+	
+	// ** Prettier version...but it doesn't work at this time...and I ran out of time to debug **
+//	byte getMoveScore (){
+//		byte mScore = 0;																			// Adjacent moves gain 2 points, diagonal moves gain 1 point
+//		int token = CURRSTATE[ROW][COL];															// Current players token
+//		
+//		// ** Move is in the interior **
+//		if (ROW > 0 && ROW < 8 && COL > 0 && COL < 8){												
+//			for (int i = ROW - 1; i < ROW + 2; i++){
+//				for (int j = COL - 1; j < COL + 2; j++){
+//					if (checkRedundant(i, j)) { continue; }
+//					mScore = (byte) (mScore + pointsEarned(i, j, token));
+//				}
+//			}
+//			return newScore(token, mScore);
+//		}
+//		
+//		// ** Move is in bottom row but not in the corners
+//		else if (ROW == 0 && COL != 0 && COL != 8){
+//			for (int i = ROW; i < ROW + 2; i++){
+//				for (int j = COL - 1; j < COL + 2; j++){
+//					if (checkRedundant(i, j)) { continue; }
+//					mScore = (byte) (mScore + pointsEarned(i, j, token));
+//				}
+//			}
+//			return newScore(token, mScore); 
+//		}
+//
+//		// ** Move is in bottom left corner **
+//		else if (ROW == 0 && COL == 0){
+//			for (int i = ROW; i < ROW + 2; i++){
+//				if (checkRedundant(i, COL + 1)) { continue; }
+//				mScore = (byte) (mScore + pointsEarned(i, COL + 1, token));
+//			}
+//			return newScore(token, mScore); 
+//		}
+//
+//		// ** Move is in bottom right corner **
+//		else if (ROW == 0 && COL == 8){
+//			for (int i = ROW; i < ROW + 2; i++){
+//				if (checkRedundant(i, COL - 1)) { continue; }
+//				mScore = (byte) (mScore + pointsEarned(i, COL - 1, token));
+//			}
+//			return newScore(token, mScore); 
+//		}
+//
+//		// ** Move is in top row but not a corner **
+//		else if (ROW == 8 && COL != 0 && COL != 8){
+//			for (int i = ROW - 1; i < ROW + 1; i++){
+//				for (int j = COL - 1; j < COL + 2; j++){
+//					if (checkRedundant(i, j)) { continue; }
+//					mScore = (byte) (mScore + pointsEarned(i, j, token));
+//				}
+//			}
+//			return newScore(token, mScore); 
+//		}
+//
+//		// ** Move is in top left corner **
+//		else if (ROW == 8 && COL == 0){
+//			for (int i = ROW - 1; i < ROW + 1; i++){
+//				for (int j = COL; j < COL + 2; j++){
+//					if (checkRedundant(i, j)) { continue; }
+//					mScore = (byte) (mScore + pointsEarned(i, j, token));
+//				}
+//			}
+//			return newScore(token, mScore); 
+//		}
+//
+//		// ** Move is in top right corner **
+//		else if (ROW == 8 && COL == 8){
+//			for (int i = ROW - 1; i < ROW + 1; i++){
+//				for(int j = COL - 1; j < COL + 1; j++){
+//					if (checkRedundant(i, j)) { continue; }
+//					mScore = (byte) (mScore + pointsEarned(i, j, token));
+//				}
+//			}
+//			return newScore(token, mScore);
+//		}
+//
+//		// ** Move is on left edge but not in a corner **
+//		else if (ROW != 0 && ROW != 8 && COL == 0){
+//			for (int i = ROW - 1; i < ROW + 2; i++){
+//				for (int j = COL; j < COL + 2; j++){
+//					if (checkRedundant(i, j)) { continue; }
+//					mScore += pointsEarned(i, j, token);
+//				}
+//			}
+//			return newScore(token, mScore); 
+//		}
+//
+//		// ** Move is on the right edge but not a corner
+//		else {
+//			for (int i = ROW - 1; i < ROW + 2; i++){
+//				for (int j = COL - 1; j < COL +1; j++){
+//					if (checkRedundant(i, j)) { continue; }
+//					mScore = (byte) (mScore + pointsEarned(i, j, token));	
+//				}
+//			}
+//			return newScore(token, mScore); 
+//		}
+//	}
+//
+//	
+//	// ** Returns true if the tile shouldn't be scored **
+//	boolean checkRedundant(int i, int j){
+//		return (ROW == i && COL == j) || (ROW - 1 == i && COL == j);
+//	}
+//	
+//	// ** Returns the total new score as positive for the AI or negative for the human **
+//	byte newScore (int token, byte mScore){
+//		if (token == 1){ 
+//			return mScore; 
+//		} else { 
+//			return (byte) (0 - mScore); 
+//		} 
+//	}
+//	
+//	// ** Returns points earned by this move **
+//	byte pointsEarned(int i, int j, int token){
+//		byte points = 0;
+//		if (CURRSTATE[i][j] == token){
+//			if (Math.abs(i - ROW) == 1 && Math.abs(j - COL) == 1){
+//				points = (byte) (points + 1);
+//			} else {
+//				points = (byte) (points + 2);
+//			}
+//		}
+//		return points;
+//	}
+}
---
+++ playSimacogo.java
@@ -0,0 +1,266 @@
+package simacogo;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+//****************************//
+//          Time per Move     //
+//  		 (3 turn avg)	  // 
+//	9-ply:    < 2 sec         // 
+//  10-ply:     6 sec         // 
+//  11-ply:    15 sec         // 
+//  12-ply:    44 sec         //  
+//  13-ply:   175 sec         //
+//  14-ply:   505 sec         //
+//****************************//
+
+
+public class playSimacogo {
+	
+	
+	// ** Generate Moves ** 	
+	public static LinkedList <Simacogo2> getMoves (Simacogo2 gameState, boolean aiPlayer){			// Creates a Linked List of all legal moves from this state	
+		byte[][] board = gameState.CURRSTATE;														// Stores current token positions
+		byte gameScore = gameState.SCORE;															// Stores current games score
+		LinkedList <Simacogo2> moves = new LinkedList<Simacogo2>();									// Stores legal moves from current state
+		byte token = 9;
+		if (aiPlayer){
+			token = 1;																				// AI Player drops 1s
+		} else {
+			token = 0;																				// Human Player drops 0s
+		}
+		
+		for (short column = 0; column < 9; column++){												// Check each column for the lowest empty space
+			byte[][] modified = new byte[9][9];
+			
+			for (short i = 0; i < 9; i++){															// Creates a copy of 2D array so we don't modify our original n^2 ouch!
+				for (short j = 0; j < 9; j++){														// Without a deep copy all moves equal the last modification
+					modified[i][j] = board[i][j];
+				}
+			}
+			
+			if (board[8][column] != 9){																// Avoid searching full columns  
+				continue;																					
+			}
+			if (board[0][column] == 9){																// Check the 0th row next 
+				modified[0][column] = token;
+				Simacogo2 move = new Simacogo2(modified, gameScore, (byte) 0, (byte) 0);			// Place token
+				moves.add(move); 
+				continue;
+			}
+			if (board[4][column] == 9){																// Open space is between 1-4 search index 1-4 
+				for (short row = 1; row < 5; row++){
+					if (board[row][column] == 9){													// Generate move in first empty space
+						modified[row][column] = token;
+						Simacogo2 move = new Simacogo2(modified, gameScore, 
+													   (byte) row, (byte) column);
+						moves.add(move);
+						break;
+					}
+				}
+			}
+			else{																					// Open space is between 5-8 search index 5-8 
+				for (short row = 5; row < 9; row++){													
+					if (board[row][column] == 9){													// Generate move in first empty space
+						modified[row][column] = token;
+						Simacogo2 move = new Simacogo2(modified, gameScore, 
+													   (byte) row, (byte) column);
+						moves.add(move);
+						break;
+					}
+				}
+			}
+		}
+		return moves;
+	}
+
+	
+	
+	// ** Search for Best Move**
+	public static Simacogo2 selectAiMove (Simacogo2 gameState, byte depth, byte endGame){			// Calculates best move for the AI player, considering a number of turns = depth
+		if (depth + endGame >=  81){																// Cap search depth if it would go beyond the 81st move
+			depth = (byte) (81 - endGame);
+		}
+		
+		short bestValue = -32000;
+		Simacogo2 bestMove = null;
+		LinkedList <Simacogo2> moves = getMoves(gameState, true);
+
+		
+		while (moves.size() > 0){
+			Simacogo2 move = moves.pop();
+			short moveValue = alphaBeta(move, (byte) (depth -1), 
+						Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, true);
+			bestValue = (short) Math.max(moveValue, bestValue);
+			if (bestValue == moveValue){
+				bestMove = move;
+			}
+		}
+		return bestMove;
+	}
+	
+	
+	
+	public static short alphaBeta(Simacogo2 gameState, byte depth, double alpha, 							
+								  double beta, boolean aiPlayer){
+		if (depth <= 0){
+			return gameState.SCORE;
+			
+		} else {
+			LinkedList <Simacogo2> moves = getMoves(gameState, aiPlayer);
+
+			
+			if (aiPlayer == true){
+				while (moves.size() > 0){
+					Simacogo2 move = moves.pop();
+					alpha = Math.max(alpha, alphaBeta(move, (byte) (depth -1), 						// Choose the best option the minimizing player would allow
+							                          alpha, beta, false)); 				
+					
+					if (beta <= alpha){
+						break;
+					}
+				}
+				return (short) alpha;
+			} else {
+				while (moves.size() > 0){
+					Simacogo2 move = moves.pop();
+					beta = Math.min(beta, alphaBeta(move, (byte) (depth -1), 						// Choose the best option the maximizing player would allow
+													alpha, beta, true));
+
+					if (beta <= alpha){
+						break;
+					}
+				}
+				return (short) beta;
+			}
+		}
+	}
+	
+	
+	
+	// ** Human Turn Handlers**
+	
+	// ** Determines if the human players move is legally allowed
+	public static boolean moveIsLegal (Simacogo2 gameState, byte row, byte column){
+		if (row > 8 || column > 8 || row < 0 || column < 0){										// Move is off the board, this move isn't legal
+			return false;
+		}
+		if (gameState.CURRSTATE[row][column] != 9) {												// A token is here, this move isn't legal
+			return false;
+		} 
+		if (row != 0 && gameState.CURRSTATE[row-1][column] == 9){  									// The space below is empty, this move isn't legal
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
+	// ** Makes legal move or returns old move **
+	public static Simacogo2 takeTurn (Simacogo2 gameState, byte row, byte column){
+		if (moveIsLegal(gameState, row, column)){													// If the move is legal, make the move
+			gameState.CURRSTATE[row][column] = 0; 
+		}
+		Simacogo2 move = new Simacogo2 (gameState.CURRSTATE, gameState.SCORE, row, column);			// Create and return new game state
+		return move;
+	}
+	
+	
+	
+	// ** Creates starting game board **
+	static byte[][] setup(){
+		byte [][] state = new byte[9][9];
+		for (short i = 0; i < 9; i++){
+			for (short j = 0; j < 9; j++){
+				state[i][j] = 9;
+			}
+		}
+		return state;
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// ** Initialize Game **
+		Scanner scanner = new Scanner (System.in);
+		byte endGame = 0;
+		byte[][] state = setup();
+		Simacogo2 start = new Simacogo2(state, (byte) 0, (byte) 0, (byte) 0);						
+		start.SCORE = 0;																			// Removes incorrect points generated during creation
+		Simacogo2 currentGame = start;
+		boolean aiPlayer = false;
+		boolean humanTurnOver = false;
+	
+		
+		
+		// ** Set difficulty of AI **
+		System.out.println("How many steps ahead should the computer look? ");
+		byte depth = scanner.nextByte();
+	
+	
+	
+		//** Main Game Loop **
+		System.out.println("---------------------------------------------------");
+		while (endGame < 81){
+			System.out.println("---------------------------------------------------");
+			System.out.println("Move #: " + endGame);
+			System.out.println("The current score is: " + currentGame.SCORE);	
+			currentGame.printBoard();																// Display current board
+			
+			// ** Humans Turn **
+			if (aiPlayer == false){																	// Humans Turn
+				System.out.println("Please enter the column of your move: ");
+				byte column = scanner.nextByte();
+				System.out.println("Please enter the row of your move: ");
+				byte row = scanner.nextByte();
+			
+
+				while (humanTurnOver == false){													
+					if (moveIsLegal(currentGame, row, column)){										// If the move is legal then make it so...Picard Style		
+						Simacogo2 move = takeTurn(currentGame, row, column);						// Enacts the players choice of move
+						currentGame = move;
+						humanTurnOver = true;														// Human player is done
+						aiPlayer = true;															// It's now the AI's turn
+						System.out.println("---------------------------------------------------");
+					} else {																		// Human player gave an illegal move make them pick again
+						System.out.println("That move is unavailable please choose again.");
+						System.out.println("Please enter the column of your move: ");
+						column = scanner.nextByte();
+						System.out.println("Please enter the row of your move: ");
+						row = scanner.nextByte();
+					}
+				}
+			// ** AIs Turn **
+			} else {																				// AIs Turn
+				long startTime = System.currentTimeMillis();										// Starting timer
+				if ((endGame + depth) >= 81){														// Prevents AI from looking for moves that can't exist
+					depth = (byte) (81 - endGame);
+				}
+				Simacogo2 aiMove = selectAiMove(currentGame, depth, endGame);						// Get AI's move
+				currentGame = aiMove;																// Make AI's move
+				aiPlayer = false;																	// aiPlayer's turn is over
+				humanTurnOver = false;																// Prepare for human turn
+				long elapsedTime = (new Date()).getTime() - startTime;								// Calculate time of search
+				System.out.println("AI Time to select move: " + elapsedTime / 1000 + " sec");		// Print out the time taken
+				System.out.println("---------------------------------------------------");
+			}					
+			endGame++;
+		}
+	
+		
+		
+		// ** Win Lose or Draw **	
+		currentGame.printBoard();
+		if (currentGame.SCORE < 0){																	// Tell the player whether they win,lose, or tie 
+			System.out.println("You WIN!");
+		} else if (currentGame.SCORE == 0){
+			System.out.println("You TIED!");
+		} else {
+			System.out.println("The mighty computer has defeated you mwahahaha!");
+		}
+		scanner.close();
+	}
+}
